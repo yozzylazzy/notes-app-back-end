970fc446288c3d3ed09b668af275d3d9def17e32
Yozzy Lazzy <77936231+yozzylazzy@users.noreply.github.com>
2023-06-22 13:15:07 +0700

    Update Fundamental Backend JS DIcoding

--- src/server.js
+++ src/server.js
@@ -1,19 +1,26 @@
 const Hapi = require('@hapi/hapi');
-const routes = require('./routes');
+const notes = require('./api/notes');
+const NotesService = require('./services/inMemory/NotesService');
 
 const init = async () => {
+	const notesService = new NotesService();
+
 	const server = Hapi.server({
 		port: 5000,
 		host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
 		routes: {
 			cors: {
 				origin: ['*'],
-				credentials: true
 			},
 		},
 	});
 
-	server.route(routes);
+	await server.register({
+		plugin: notes,
+		options: {
+			service: notesService,
+		}
+	});
 
 	await server.start();
 	console.log(`Server berjalan pada ${server.info.uri}`);
