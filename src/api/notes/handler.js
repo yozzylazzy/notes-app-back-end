92cb69977435cec2e5976741a6c996222e8e3548
Yozzy Lazzy <kristian_yosef@yahoo.com>
2023-07-12 11:13:46 +0700

    authorization implemented

--- src/api/notes/handler.js
+++ src/api/notes/handler.js
@@ -16,7 +16,12 @@ class NotesHandler {
     try {
       this._validator.validateNotePayload(request.payload);
       const {title = 'untitled', body, tags} = request.payload;
-      const noteId = await this._service.addNote({title, body, tags});
+      const {id: credentialId} = request.auth.credentials;
+
+      const noteId = await this._service.addNote({
+        title, body, tags,
+        owner: credentialId,
+      });
 
       const response = h.response({
         status: 'success',
@@ -47,8 +52,9 @@ class NotesHandler {
     }
   }
 
-  async getNotesHandler() {
-    const notes = await this._service.getNotes();
+  async getNotesHandler(request) {
+    const {id: credentialId} = request.auth.credentials;
+    const notes = await this._service.getNotes(credentialId);
     return {
       status: 'success',
       data: {
@@ -60,6 +66,8 @@ class NotesHandler {
   async getNoteByIdHandler(request, h) {
     try {
       const {id} = request.params;
+      const {id: credentialId} = request.auth.credentials;
+      await this._service.verifyNoteOwner(id, credentialId);
       const note = await this._service.getNoteById(id);
       return {
         status: 'success',
@@ -91,7 +99,8 @@ class NotesHandler {
     try {
       this._validator.validateNotePayload(request.payload);
       const {id} = request.params;
-
+      const {id: credentialId} = request.auth.credentials;
+      await this._service.verifyNoteOwner(id, credentialId);
       await this._service.editNoteById(id, request.payload);
       return {
         status: 'success',
@@ -120,6 +129,9 @@ class NotesHandler {
   async deleteNoteByIdHandler(request, h) {
     try {
       const {id} = request.params;
+      const {id: credentialId} = request.auth.credentials;
+
+      await this._service.verifyNoteOwner(id, credentialId);
       await this._service.deleteNoteById(id);
       return {
         status: 'success',
