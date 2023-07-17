2eb19d76fb55a090a71759f563339644269f5b6b
Yozzy Lazzy <kristian_yosef@yahoo.com>
2023-07-18 03:10:59 +0700

    Update

--- src/server.js
+++ src/server.js
@@ -26,10 +26,10 @@ const CollaborationsService = require('./services/postgres/CollaborationsService
 const CollaborationsValidator = require('./validator/collaborations');
 
 const init = async () => {
-  const notesService = new NotesService();
+  const collaborationsService = new CollaborationsService();
+  const notesService = new NotesService(collaborationsService);
   const usersService = new UsersService();
   const authenticationsService = new AuthenticationsService();
-  const collaborationsService = new CollaborationsService();
 
   const server = Hapi.server({
     port: process.env.PORT,
