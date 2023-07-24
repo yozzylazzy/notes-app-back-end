bfab83d57b13019b563eee1c071fdafc84e2170c
Yozzy Lazzy <kristian_yosef@yahoo.com>
2023-07-25 01:26:48 +0700

    local storage implemented

--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 
 const Hapi = require('@hapi/hapi');
 const Jwt = require('@hapi/jwt');
+const Inert = require('@hapi/inert');
 const path = require('path');
 
 // Notes
@@ -56,6 +57,9 @@ const init = async () => {
   await server.register([
     {
       plugin: Jwt,
+    },
+    {
+      plugin: Inert,
     },
   ]);
 
